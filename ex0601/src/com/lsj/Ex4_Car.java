337bab4ab6a5bd71c67817e9d722b5a9d11c6abc
LSJ <1007ckddjs12@gmail.com>
2026-06-01 18:01:43 +0900

    feat: add var

--- ex0601/src/com/lsj/Ex4_Car.java
+++ ex0601/src/com/lsj/Ex4_Car.java
@@ -4,12 +4,13 @@ public class Ex4_Car {
 
 	public static void main(String[] args) {
 		
-		Car car = new Car();
-		car.setBrand("현대");
+		Car hyundai = new Car("현대");
+		
+		Car ford = new Car("포드", "머스탱", 2015);
 		
 		//차의 브랜드를 출력하세요.
-		System.out.println(car.getBrand());
-
+		System.out.println(hyundai.getBrand());
+		
+		ford.printInfo();
 	}
-
 }
